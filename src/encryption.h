fc1cca2cc3d709b5b5c2764080f77fe22b425970
Andrey Arthur <andrey_arthur1@outlook.com>
2024-10-06 16:10:52 -0300

    document encryption functions

--- src/encryption.h
+++ src/encryption.h
@@ -4,10 +4,34 @@
 #include "aes.h"
 #include <stddef.h>
 
+/*
+ * \brief Calculates the length of the encrypted string based on the plaintext
+ * length.
+ *
+ * \param plaintext_len: The length of the plaintext.
+ *
+ * \return The length of the encrypted string.
+ */
 size_t CalculateEncryptedLen(size_t plaintext_len);
 
+/*
+ * \brief Encrypts a string to a base64 ciphertext.
+ *
+ * \param string The original message.
+ * \param key_string The encryption key as string.
+ * \param buf The decrypted buffer, the function assumes that it has sufficient
+ * length.
+ */
 void EncryptString(char *string, char *key_string, char *buf);
 
+/*
+ * \brief Decrypts an encrypted base64 string.
+ *
+ * \param string The encrypted base64 string.
+ * \param key_string The encryption key as string.
+ * \param buf The decrypted buffer, the function assumes that it has sufficient
+ * length.
+ */
 void DecryptString(char *string, char *key_string, char *buf);
 
 #endif
